9ca0019497ed85fdbb700bbd33aeb9d4c20a59e0
radselenium <ajaik@radiant-solns.com>
2024-10-22 08:26:36 +0530

    Update config.js

--- client/src/components/config/config.js
+++ client/src/components/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	PORT: process.env.PORT || 7001,
-	URL: process.env.URL || 'http://localhost:'	
+	URL: process.env.URL || 'https://black-smoke-0ea12170f.5.azurestaticapps.net'	
 }
